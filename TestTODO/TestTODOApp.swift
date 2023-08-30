24db8d0a629a0b23f7de9dfded6aaf004344033f
albertkristian <albert19781kristian@gmail.com>
2023-08-30 17:12:09 +0300

    Initial Commit

---
+++ TestTODO/TestTODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTODOApp.swift
+//  TestTODO
+//
+//  Created by Albert Kristian on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
