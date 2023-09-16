a9801df71de2f3a56925f1121d42379b819eee63
albertkristian <albert19781kristian@gmail.com>
2023-09-16 19:27:10 +0300

    Reworked UI + fixed some remarks

---
+++ TestTODO/UI/Custom/CheckBoxComponent.swift
@@ -0,0 +1,25 @@
+//
+//  CheckBoxComponent.swift
+//  TestTODO
+//
+//  Created by Albert Kristian on 11.09.2023.
+//
+
+import SwiftUI
+
+struct CheckBoxComponent: View {
+    let isChecked: Bool
+    
+    var body: some View {
+            Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+                .foregroundColor(isChecked ? Color(UIColor.systemBlue) : Color.secondary)
+        }
+}
+
+struct CheckBoxComponent_Previews: PreviewProvider {
+
+    static var previews: some View {
+        CheckBoxComponent(isChecked: false)
+        CheckBoxComponent(isChecked: true)
+    }
+}
---
+++ TestTODO/ViewModels/TodoViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  TodoViewModel.swift
+//  TestTODO
+//
+//  Created by Albert Kristian on 03.09.2023.
+//
+
+import Foundation
+
+extension TodoView {
+    @MainActor class TodoViewModel: ObservableObject {
+        @Published var dummyData = [any TodoModel]()
+        
+        init() {
+            for i in 0...15 {
+                dummyData.append(RealmTodoModel(content: "Item number: \(i)"))
+            }
+        }
+        
+        func deleteAt(at offsets: IndexSet) {
+            dummyData.remove(atOffsets: offsets)
+        }
+        
+        func check(id: String) {
+            let index = dummyData.firstIndex(where: { $0.id == id })
+            dummyData[index!].modify(isChecked: !dummyData[index!].isChecked)
+            
+//            dummyData = dummyData.map({ model in
+//                if (model.id == id) {
+//                    var tempModel = model
+//                    tempModel.modify(isChecked: !model.isChecked)
+//                    return tempModel
+//                } else {
+//                    return model
+//                }
+//            })
+        }
+    }
+}
